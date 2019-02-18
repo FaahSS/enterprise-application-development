54a25799a37b96433f21e0f9cea877605083827f
FaahSS <faahsalgarella@gmail.com>
2019-02-18 09:48:12 -0300

    CRUD e Exercicio

--- 03-JPA-Introducao/src/br/com/fiap/main/CadastroTeste.java
+++ 03-JPA-Introducao/src/br/com/fiap/main/CadastroTeste.java
@@ -1,10 +1,16 @@
 package br.com.fiap.main;
 
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
-public class Teste {
+import br.com.fiap.entity.Cliente;
+import br.com.fiap.entity.Genero;
+
+public class CadastroTeste {
 	public static void main(String[] args) {
 		//Criar um gerenciador de entidades:
 		//Primeiro cria a Fábrica
@@ -12,6 +18,16 @@ public class Teste {
 		
 		//Depois a fabrica cria os Entity Manager
 		EntityManager em = fabrica.createEntityManager();
+		
+		//Instanciar o Cliente
+		Cliente cliente = new Cliente("Fabio", new GregorianCalendar(2000, Calendar.MARCH, 2), 
+				"123.123.123-99", Genero.MASCULINO, null, true);
+		//Cadastrar o cliente
+		em.persist(cliente);
+		
+		//Abre uma transação e finaliza com commit
+		em.getTransaction().begin();
+		em.getTransaction().commit();
 		
 		em.close();
 		fabrica.close();
